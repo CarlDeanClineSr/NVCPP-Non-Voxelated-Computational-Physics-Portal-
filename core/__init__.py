1f338dd009155980e71065a19640872286d7d9f3
Carl Dean Cline Sr <CARLDCLINE@GMAIL.COM>
2026-08-26 03:02:11 +0000

    feat: publish audited NVCPP core foundation

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+"""Audited and fail-closed computational physics primitives for NVCPP."""
+
+from .cline_l1_chain_v1 import PROTOCOL_ID, PROTOCOL_VERSION, ProtocolConfig
+
+__all__ = ["PROTOCOL_ID", "PROTOCOL_VERSION", "ProtocolConfig"]
